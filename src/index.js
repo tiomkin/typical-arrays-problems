206dbe2dc0dfbada59cbe660a6c64a163b0d70ce
Artsiom Burdukou <artsiomburdukou@gmail.com>
2021-08-23 16:20:46 +0300

    feat: implement methods in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == null || array.length == 0) {
+      return 0;
+  }
+
+  let min = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+          min = array[i];
+      }
+  }
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+  
+    let max = array[0];
+  
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+
+    let sum = array.reduce(function (sum, current) {
+        return sum + current;
+    }, 0);
+
+    return sum / array.length;
 }
